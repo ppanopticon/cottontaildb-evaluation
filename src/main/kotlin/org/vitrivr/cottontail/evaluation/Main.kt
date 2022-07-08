b5def7100492597b8cf8cf895765b955ce0e902c
Ralph Gasser <rg@pontius.ch>
2022-07-08 11:43:11 +0200

    Initial commit

---
+++ src/main/kotlin/org/vitrivr/cottontail/evaluation/Main.kt
@@ -0,0 +1,29 @@
+package org.vitrivr.cottontail.evaluation
+
+import io.grpc.ManagedChannelBuilder
+import org.vitrivr.cottontail.client.SimpleClient
+import java.nio.file.Paths
+
+/**
+ * Host and port of Cottontail DB instance that should be tested. Please adjust.
+ */
+private val host = "127.0.0.1"
+private val port = 1865
+
+/** The path to the working directory. */
+private val workingdir_str = "/Users/rgasser/Downloads/"
+
+/** The [ManagedChannel] to used to access Cottontail DB. */
+private val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+
+/** The simple client used by Cottontail DB. */
+val client = SimpleClient(channel)
+
+/** The simple client used by Cottontail DB. */
+val workingdir = Paths.get(workingdir_str)
+
+/**
+ *
+ */
+fun main() {
+}
