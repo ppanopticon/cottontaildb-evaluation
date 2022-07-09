92351f26ca562da6873a644e4c584c2ee7149465
Ralph Gasser <rg@pontius.ch>
2022-07-09 16:52:11 +0200

    Adjusted plot and runtime script to separate data creation from data export and plotting.

--- src/main/kotlin/org/vitrivr/cottontail/evaluation/Main.kt
+++ src/main/kotlin/org/vitrivr/cottontail/evaluation/Main.kt
@@ -1,12 +1,14 @@
 package org.vitrivr.cottontail.evaluation
 
 import io.grpc.ManagedChannelBuilder
-import jetbrains.letsPlot.elementText
 import jetbrains.letsPlot.theme
 import org.vitrivr.cottontail.client.SimpleClient
 import org.vitrivr.cottontail.evaluation.datasets.prepare
+import org.vitrivr.cottontail.evaluation.runtime.plot
 import org.vitrivr.cottontail.evaluation.runtime.runtime
+import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 /**
  * Host and port of Cottontail DB instance that should be tested. Please adjust.
@@ -15,7 +17,7 @@ private val host = "127.0.0.1"
 private val port = 1865
 
 /** The path to the working directory. */
-private val workingdir_str = "/home/tank/evaluation"
+private val basepath = "/home/tank/evaluation"
 
 /** The [ManagedChannel] to used to access Cottontail DB. */
 private val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
@@ -23,20 +25,37 @@ private val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(
 /** The simple client used by Cottontail DB. */
 val client = SimpleClient(channel)
 
-/** The simple client used by Cottontail DB. */
-val workingdir = Paths.get(workingdir_str)
-
 /** Default theme for plotting. */
 val plotTheme = theme()
 
+/** The current timestamp. */
+val timestamp = System.currentTimeMillis()
+
+/** Path to the working directory. */
+val workingdir = Paths.get(basepath)
+
+/** Path to the output directory. */
+val outdir = workingdir.resolve("out/$timestamp")
 
 /**
  *
  */
 fun main(args: Array<String>) {
+    /* Create working directory. */
+    if (!Files.exists(workingdir)) {
+        println("Failed to create working directory under $workingdir")
+        exitProcess(1)
+    }
+
+    /* Evaluate command to execute. */
     when (val command = args[0]) {
         "prepare" -> prepare()
         "runtime" -> runtime()
+        "plot" -> {
+            val name = args[1]
+            val path = Paths.get(args[2])
+            plot(name, path)
+        }
         else -> {
             println("Unknown command '${command}'.")
         }
--- src/main/kotlin/org/vitrivr/cottontail/evaluation/runtime/YandexDeep1B.kt
+++ src/main/kotlin/org/vitrivr/cottontail/evaluation/runtime/YandexDeep1B.kt
@@ -14,39 +14,90 @@ import kotlin.system.measureTimeMillis
 /**
  * Prepares the Yandex DEEP 1B in Cottontail DB.
  */
-fun runYandexDeep1BSingle(k: Int = 10, warmup: Int = 1, iterations: Int = 10): List<Float> {
-    val results = mutableListOf<Float>()
-    val bar = ProgressBarBuilder().setInitialMax((warmup + iterations).toLong()).setStyle(ProgressBarStyle.ASCII).setTaskName("Running Benchmark (Yandex DEEP 1B, Brute-force, single-threaded)").build()
+fun runYandexDeep1BSingle(k: Int = 10, warmup: Int = 1, iterations: Int = 10, into: Map<String,MutableList<*>>) {
+    val bar = ProgressBarBuilder().setInitialMax((warmup + iterations).toLong()).setStyle(ProgressBarStyle.ASCII).setTaskName("Running Benchmark (Yandex DEEP 1B, brute-force, single-threaded)").build()
     YandexDeep1BIterator(workingdir.resolve("datasets/yandex-deep1b/query.public.10K.fbin")).use {
-
         /* Warmup  queries. */
         bar.setExtraMessage("Warmup...")
         for (w in 0 until warmup) {
             val (_, feature) = it.next()
-            val query = Query("evaluation.yandex_deep1b")
-                .distance("feature", feature, Distances.L2, "distance")
-                .order("distance", Direction.ASC)
-                .limit(k.toLong())
-                .disallowParallelism()
-            client.query(query).forEach { _ -> /* No op. */ }
+            warmup(k, feature)
             bar.step()
         }
 
         /* Benchmark queries. */
         bar.setExtraMessage("Benchmark...")
         for (w in 0 until iterations) {
+            /* Indicate progress. */
+            bar.step()
+
             val (_, feature) = it.next()
             val durationMs = measureTimeMillis {
-                val query = Query("evaluation.yandex_deep1b")
+                val query = Query().from(Query("evaluation.yandex_deep1b").limit(1_000_000))
                     .distance("feature", feature, Distances.L2, "distance")
                     .order("distance", Direction.ASC)
                     .limit(k.toLong())
                     .disallowParallelism()
-                client.query(query).forEach { _ -> /* No op. */ }
+                client.query(query).forEach { /* No op. */ }
             }
-            results.add(durationMs / 1000.0f)
+
+            /* Record data. */
+            (into[CATEGORY_KEY] as MutableList<String>).add("Yandex Deep1B (brute-force, single-threaded)")
+            (into[RUN_KEY] as MutableList<Int>).add(w + 1)
+            (into[K_KEY] as MutableList<Int>).add(k)
+            (into[RUNTIME_KEY] as MutableList<Double>).add(durationMs / 1000.0)
+
+            /* Indicate progress. */
             bar.step()
         }
     }
-    return results
+}
+
+/**
+ * Prepares the Yandex DEEP 1B in Cottontail DB.
+ */
+fun runYandexDeep1BParallel(k: Int = 10, warmup: Int = 1, iterations: Int = 10, into: Map<String,MutableList<*>>) {
+    val bar = ProgressBarBuilder().setInitialMax((warmup + iterations).toLong()).setStyle(ProgressBarStyle.ASCII).setTaskName("Running Benchmark (Yandex DEEP 1B, Brute-force, multi-threaded)").build()
+    YandexDeep1BIterator(workingdir.resolve("datasets/yandex-deep1b/query.public.10K.fbin")).use {
+        /* Warmup  queries. */
+        bar.setExtraMessage("Warmup...")
+        for (w in 0 until warmup) {
+            val (_, feature) = it.next()
+            warmup(k, feature)
+            bar.step()
+        }
+
+        /* Benchmark queries. */
+        bar.setExtraMessage("Benchmark...")
+        for (w in 0 until iterations) {
+            /* Indicate progress. */
+            bar.step()
+
+            val (_, feature) = it.next()
+            val durationMs = measureTimeMillis {
+                val query = Query().from(Query("evaluation.yandex_deep1b").limit(1_000_000))
+                    .distance("feature", feature, Distances.L2, "distance")
+                    .order("distance", Direction.ASC)
+                    .limit(k.toLong())
+                client.query(query).forEach { /* No op. */ }
+            }
+
+            /* Record data. */
+            (into[CATEGORY_KEY] as MutableList<String>).add("Yandex Deep1B (brute-force, parallel)")
+            (into[RUN_KEY] as MutableList<Int>).add(w + 1)
+            (into[K_KEY] as MutableList<Int>).add(k)
+            (into[RUNTIME_KEY] as MutableList<Double>).add(durationMs / 1000.0)
+        }
+    }
+}
+
+/**
+ * Executes a warmup query.
+ */
+private fun warmup(k: Int, feature: FloatArray) {
+    val query = Query("evaluation.yandex_deep1b")
+        .distance("feature", feature, Distances.L2, "distance")
+        .order("distance", Direction.ASC)
+        .limit(k.toLong())
+    client.query(query).forEach { _ -> /* No op. */ }
 }
